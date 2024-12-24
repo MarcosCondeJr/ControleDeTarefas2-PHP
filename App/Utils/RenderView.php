a0e5c0c428eac8002d414455b73aa635020bc440
marcoscondejr <marcos14conde@hotmail.com>
2024-12-24 15:56:17 -0300

    refactor: alterando o diretorio das views na classe render

--- App/Utils/RenderView.php
+++ App/Utils/RenderView.php
@@ -4,9 +4,9 @@ namespace App\Utils;
 
 class RenderView 
 {
-    public static function loadView($view, $data)
+    public static function loadView($pasta, $view, $data)
     {
         extract($data);
-        require_once __DIR__ . "/../Views/{$view}.php";
+        require_once __DIR__ . "/../Views/{$pasta}/{$view}.php";
     }
 }
