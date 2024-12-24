8ffbcc6dd9e8cd3938ab2af2710f6711c79c0b04
marcoscondejr <marcos14conde@hotmail.com>
2024-12-23 22:43:21 -0300

    feat: Concluindo sistema de rotas

--- Core/Core.php
+++ Core/Core.php
@@ -1,29 +1,42 @@
 <?php
 
+use App\Controllers\NotFoundController;
+
 class Core 
 {
     public function run($routes)
     {
         $url = '/';
-        var_dump($routes);
 
         isset($_GET['url']) ? $url .= $_GET['url'] : '';
 
+        ($url != '/') ? $url = rtrim($url, '/') : $url;
+
+        $routerFound = false;
+
         foreach($routes as $path => $controller)
         {
-            $pattern = '#^' . preg_replace('/{id}/', '(\w+)', $path). '$#';
+            $pattern = '#^' . preg_replace('/{id}/', '([a-zA-Z0-9-_?]+)', $path) . '$#';
 
             if(preg_match($pattern, $url, $matches))
             {
                 array_shift($matches);
+
+                $routerFound = true;
 
                 [$currentController, $action] = explode('@', $controller);
 
                 $qualifiedController = "App\\Controllers\\{$currentController}";
                 
                 $newController = new $qualifiedController();
-                $newController->$action();
+                $newController->$action($matches);
             }
+        }
+
+        if(!$routerFound)
+        {
+            $controller = new NotFoundController();
+            $controller->index();
         }
 
     }
