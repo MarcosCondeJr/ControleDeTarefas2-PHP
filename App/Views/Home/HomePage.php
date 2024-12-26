d4652d5e00f82f528c888d632cd15a0394efbd5e
marcoscondejr <marcos14conde@hotmail.com>
2024-12-25 23:07:00 -0300

    feat: criando estilos e alguns padrões para a home

--- App/Views/Home/HomePage.php
+++ App/Views/Home/HomePage.php
@@ -5,9 +5,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/styleHome.css">
     <title>Home</title>
 </head>
 <body>
-    <h1>Bem Vindo a Home</h1>
+    <h3 class="titulo">Olá Sr Marcos, seja Bem Vindo!</h3>
 </body>
 </html>
