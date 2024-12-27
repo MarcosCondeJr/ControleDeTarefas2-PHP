33f2f01ab2144e91228bc97b97b5caaab0ce321d
marcoscondejr <marcos14conde@hotmail.com>
2024-12-27 00:58:35 -0300

    criando o formulário de cadastro de categoria

--- App/Views/Categoria/cadastroCategoriaView.php
+++ App/Views/Categoria/cadastroCategoriaView.php
@@ -9,7 +9,26 @@
 </head>
 <body>
     <div class="container">
-        <h1>Cadastro Categoria</h1>
+        <form action="">
+            <h1 class="mt-5 mb-3">Cadastro de Categoria</h1>
+            <div class="row">
+                <div class="col-sm-2 mb-3">
+                    <label for="codigoCategoria" class="form-label">Código</label>
+                    <input style="background-color:rgb(232, 231, 231)" type="text" class="form-control" readonly>
+                </div>
+                <div class="col mb-3">
+                    <label for="nomeCategoria" class="form-label">Nome Categoria</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mb-4">
+                    <label for="ds_categoria" class="form-label">Descrição</label>
+                    <textarea name="" id="" class="form-control"></textarea>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-success">Salvar</button>
+        </form>
     </div>
 </body>
 </html>
