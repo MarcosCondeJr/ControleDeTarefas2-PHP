d99fbd62910325687bdb9c96571edb3ba8028ec2
Marcos Conde <marcos14conde@hotmail.com>
2024-12-27 14:09:09 -0300

    feat: criando a view de lista de categorias

--- App/Views/Categoria/ListCategoriaView.php
+++ App/Views/Categoria/ListCategoriaView.php
@@ -8,9 +8,34 @@
     <title>Lista Categoria</title>
 </head>
 <body>
-    <div class="container mt-4">
-        <h1>Categorias</h1>
-
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center mt-4">
+            <h1>Lista de Categorias</h1>
+            <button class="btn btn-primary ms-auto" onclick="window.location.href='<?= BASE_URL ?>/categoria/cadastro-categoria'">Cadastrar</button>
+        </div>
+        <div class="mt-3">
+            <table class="table table-striped border">
+                <thead class="text-center">
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <a href="#" class="btn btn-warning">Editar</a>
+                            <a href="#" class="btn btn-danger">Deletar</a>
+                         </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </body>
 </html>
