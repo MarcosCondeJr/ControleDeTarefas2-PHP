d311bb8fad350e4384c7b31c4b0f3780df310d0a
marcoscondejr <marcos14conde@hotmail.com>
2024-12-29 00:31:34 -0300

    feat: criando a controller e rotas de user

---
+++ App/Views/User/CadastroUserView.php
@@ -0,0 +1,15 @@
+<?php include 'App/Views/navbar.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Usuario</title>
+</head>
+<body>
+    <div class="container">
+        
+    </div>
+</body>
+</html>
