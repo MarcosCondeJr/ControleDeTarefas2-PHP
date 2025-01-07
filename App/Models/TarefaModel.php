3c1275717cc9eceb9c0f841f55ea9597bf15df21
marcoscondejr <marcos14conde@hotmail.com>
2025-01-07 20:38:03 -0300

    feat: criando a model de tarefa

--- App/Models/TarefaModel.php
+++ App/Models/TarefaModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 use App\Models\UsuarioModel;
 use App\Models\CategoriaModel;
+use App\Models\SituacaoModel;
 use PDO;
 
 
@@ -10,8 +11,77 @@ class TarefaModel
 {
     private $idTarefa;
     private $cdTarefa;
-    private UsuarioModel $idUsuario;
-    private CategoriaModel $idCategoria;
-    private $idSituacao;
+    private UsuarioModel $usuario;
+    private CategoriaModel $categoria;
+    private SituacaoModel $situacao;
     private $dsTarefa;
+    private $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+        $this->usuario = new UsuarioModel($this->db);
+        $this->categoria = new CategoriaModel($this->db);
+        $this->situacao = new SituacaoModel($this->db);
+    }
+
+    public function getIdTarefa()
+    {
+        return $this->idTarefa;
+    }
+
+    public function setIdTarefa($idTarefa)
+    {
+        $this->idTarefa = $idTarefa;
+    }
+
+    public function getCdTarefa()
+    {
+        return $this->cdTarefa;
+    }
+
+    public function setCdTarefa($cdTarefa)
+    {
+        $this->cdTarefa = $cdTarefa;
+    }
+
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    public function setUsuario(UsuarioModel $usuario)
+    {
+        $this->usuario = $usuario;
+    }
+
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    public function setCategoria(CategoriaModel $categoria)
+    {
+        $this->categoria = $categoria;
+    }
+
+    public function getSituacao()
+    {
+        return $this->situacao;
+    }
+
+    public function setSituacao(SituacaoModel $situacao)
+    {
+        $this->situacao = $situacao;
+    }
+
+    public function getDsTarefa()
+    {
+        return $this->dsTarefa;
+    }
+
+    public function setDsTarefa($dsTarefa)
+    {
+        $this->dsTarefa = $dsTarefa;
+    }
 }
